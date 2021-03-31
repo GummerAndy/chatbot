5403992701178e7969b28d440af612630b75c705
Andy Gummer <gummer.andy@gmail.com>
2021-03-31 13:31:32 +0100

    Added random new id

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import PropTypes from "prop-types";
 import { Container, Form, Button } from "react-bootstrap";
+import { v4 as uuidV4 } from "uuid";
 
 export default function Login({ onIdSubmit }) {
   const idRef = useRef();
@@ -8,6 +9,10 @@ export default function Login({ onIdSubmit }) {
   const handleSubmit = (e) => {
     e.preventDefault();
     onIdSubmit(idRef.current.value);
+  };
+
+  const createNewId = () => {
+    onIdSubmit(uuidV4());
   };
 
   return (
@@ -23,7 +28,9 @@ export default function Login({ onIdSubmit }) {
         <Button type="submit" className="mr-2">
           Login
         </Button>
-        <Button variant="secondary">Create a new Id</Button>
+        <Button onClick={createNewId} variant="secondary">
+          Create a new Id
+        </Button>
       </Form>
     </Container>
   );
